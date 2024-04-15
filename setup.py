c2cdca4c55ca21e7abac73b3436fa44f40c5cea5
Julian Hohner <daswaeldchen@gmail.com>
2024-04-15 10:46:23 +0200

    Test push

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="ResearchTikPy",
-    version="0.1.3",
+    version="0.1.5",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "requests", "pandas" 
+        "requests",
+        "pandas"
     ],
     author="Julian Hohner",
     author_email="daswaeldchen@gmail.com",
     description="Python API wrapper for the TikTok Research API",
-    keywords=["TikTok API research", "tiktok", "python3", "api", "tiktok-api", "tiktok api", "tiktok_api", "research"],
-    url="https://github.com/HohnerJulian/ResearchTikPy", 
+    keywords=["TikTok API research", "tiktok", "python3", "api", "tiktok-api", "research"],
+    url="https://github.com/HohnerJulian/ResearchTikPy",
 )
