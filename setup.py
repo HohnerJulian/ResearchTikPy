45f59f230a4c3ca4d42448e55806f1e14c80a090
Julian Hohner <59646980+HohnerJulian@users.noreply.github.com>
2024-04-15 08:33:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ResearchTikPy",
-    version="0.1.3",
+    version="0.1.4",
     license="MIT",
     packages=find_packages(),
     install_requires=[
