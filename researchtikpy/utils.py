1f09ae461e91cc180de670961e15d28dff82c1b1
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-18 11:25:37 +0100

    Add AccessToken class. This automatically refreshes the token when it expires.

--- researchtikpy/utils.py
+++ researchtikpy/utils.py
@@ -1,10 +1,84 @@
-def validate_access_token_object(access_token) -> str:
+from datetime import datetime, timedelta
+from typing import Dict, Literal, Union
+
+import requests
+
+
+class AccessToken:
+
+    def __init__(self, client_key, client_secret):
+        """Requests an access token from the TikTok API using client credentials.
+
+        Parameters
+        ----------
+        client_key : str
+            The client key provided by TikTok.
+        client_secret : str
+            The client secret provided by TikTok.
+
+        Raises
+        ------
+        Exception
+            If the request to the TikTok API fails or is not successful.
+        """
+        self.client_key = client_key
+        self.client_secret = client_secret
+        self._token_type_: Literal["Bearer"] = "Bearer"
+        self._expires_in_: int = 7200
+        self._access_token_: str = self._get_access_token_()
+
+    def is_expired(self) -> bool:
+        """Check if the access token has expired."""
+        return datetime.now() > self.expires_at
+
+    @property
+    def expires_at(self) -> datetime:
+        """Return the datetime when the access token expires."""
+        return datetime.now() + timedelta(seconds=self._expires_in_)
+
+    @property
+    def token(self) -> str:
+        """Return the currently valid access token."""
+        if self.is_expired():
+            self.refresh()
+        return self._access_token_
+
+    def refresh(self):
+        """Refresh the access token if it has expired."""
+        if self.is_expired():
+            self._get_access_token_()
+
+    def _get_access_token_(self):
+        endpoint_url = "https://open.tiktokapis.com/v2/oauth/token/"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "client_key": self.client_key,
+            "client_secret": self.client_secret,
+            "grant_type": "client_credentials",
+        }
+
+        response = requests.post(
+            endpoint_url, headers=headers, data=data, timeout=10
+        )  # TODO: Add timeout parameter
+
+        if response.status_code != 200:
+            raise ValueError(f"Failed to obtain access token: {response.text}")
+        response_json = response.json()
+        self._access_token_ = response_json.get("access_token")
+        self._expires_in_ = response_json.get("expires_in")
+        self._token_type_ = response_json.get("token_type")
+
+    def __str__(self):
+        return f"{self._token_type_}: {self.token}, expires at: {self.expires_at}"
+
+
+def validate_access_token_object(access_token: Union[AccessToken, Dict, str]) -> str:
     """
     Validate the access token object and return the access token string.
 
     Parameters
     ----------
-    access_token : str or dict
+    access_token : AccessToken, str or dict
         Access token for TikTok's API.
 
     Returns
@@ -19,7 +93,10 @@ def validate_access_token_object(access_token) -> str:
     """
     if isinstance(access_token, str):
         return access_token
-    elif isinstance(access_token, dict):
+    if isinstance(access_token, dict):
         return access_token.get("access_token")
-    else:
-        raise ValueError("Invalid access token object. Please provide a valid access token string or dictionary.")
+    if isinstance(access_token, AccessToken):
+        return access_token.token
+    raise ValueError(
+        "Invalid access token object. Please provide a valid access token string or dictionary."
+    )
